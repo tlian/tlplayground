338144b5daa7f0230eb96d5c3121fa98c4787603
Thawngzapum Lian <tlian@salesforce.com>
2017-05-17 12:06:23 -0400

    Multiple functions to onboard Q&E drives
       This commit introduced multiple functions to onboard Q&E disks.
         Windows MPIO configuration
         Initialize, Partition, Format the disks

--- onboard-qe-drives.ps1
+++ onboard-qe-drives.ps1
@@ -20,10 +20,36 @@
 
 param(
   [Int]$QdriveLunId,
-  [Int]$EdriveLunId]
+  [Int]$EdriveLunId
 )
 
 $DiskModelPrefix = "3PARdata"
+
+Function IsMpioInstalled {
+  $mpio = Get-WindowsFeature "*multipath*"
+  return $mpio.Installed
+}
+
+Function _Is3PARdataSupported {
+  # this is only a helper function
+  # MPIO configuration should be handled by a cookbook in the pipeline
+  return (Get-MSDSMSupportedHW).VendorId -contains $DiskModelPrefix
+}
+
+Function _InstallMPIOFeature {
+  # Only Helper function - should be handled by Chef Cookbook
+  Add-WindowsFeature -Name "Multipath-IO"
+}
+
+Function _Add3PARdataMpioDevice {
+  # Helper function only - should be handled by Chef cookbook
+  New-MSDSMSupportedHW -VendorId $DiskModelPrefix -ProductId "VV"
+}
+
+Function _RebootComputer {
+  # Only helper function
+  Restart-Computer
+}
 
 Function MapLunIdToDiskNumber {
   # Map LunID to correct local disk number
@@ -36,7 +62,9 @@ Function MapLunIdToDiskNumber {
   $disk = gwmi -class Win32_DiskDrive | `
           ? { $_.SCSILogicalUnit -eq $LunId -and $_.partitions -eq 0 -and $_.Model -like "$DiskModelPrefix*" }
   # The following may not be needed. PS returns everything so cast the desired value in the calling subroutine.
+  if (!($disk.Index.length -eq 1)) { throw "FAILED to map LunID to Disk Drive Number. Found zero or multiple drives!"}
   $driveNum = $disk.Index
+  return $driveNum
 }
 
 Function InitializeDisk {
@@ -50,7 +78,7 @@ Function InitializeDisk {
 Function PartitionDisk {
   Param(
     [Int]$DiskNumber,
-    [string]$DriveLetter,
+    [string]$DriveLetter
   )
   New-Partition -DiskNumber $DiskNumber -UseMaximumSize
   # New-Partition -DiskNumber $DiskNumber -AssignDriveLetter -UseMaximumSize
@@ -64,3 +92,25 @@ Function FormatDisk {
   )
   Format-Volume -DriveLetter $DriveLetter -FileSystem $FStype -NewFileSystemLabel $FSlabel -Confirm:$false
 }
+
+# Install MPIO if not already
+If (!(IsMpioInstalled)) { _InstallMPIOFeature }
+
+# Add 3PARdata device if not already
+If (!(_Is3PARdataSupported)) { 
+  _Add3PARdataMpioDevice
+  _RebootComputer
+  Start-Sleep -m 5
+}
+
+# Extract Disk number
+$QdiskNum = MapLunIdToDiskNumber -LunId $QdriveLunId
+
+# Initialize Disk
+InitializeDisk -DiskNumber $QdiskNum
+
+# Partition Disk
+PartitionDisk -DiskNumber $QdiskNum
+
+# Format Disk
+FormatDisk -DriveLetter "Q" -FSlabel "quorum"
